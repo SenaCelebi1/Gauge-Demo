4e2446b1037869b99f00b107275adaced5641805
Sena Celebi <43573954+SenaCelebi@users.noreply.github.com>
2021-11-02 21:58:16 +0300

    trendyol project is deleted, scenario is edited.

---
+++ gauge-modanisa/specs/buy_lipstick.spec
@@ -0,0 +1,11 @@
+# Tests the process to buy lipstick from modanisa
+
+## By the lipstick in modanisa
+* Open the product page with "https://www.modanisa.com/velvet-lipstick-6li-mini-kapsul--golden-rose.html?ck=1-tr-TRY-lkij-seditor:desc"
+* Add product to basket
+* View the basket and checkout "https://www.modanisa.com/basket/"
+
+## By the lipstick
+* Open the product page with "https://www.trendyol.com/alix-avien/kahverengi-ruj-matte-lipstick-202-p-88497443?boutiqueId=580743&merchantId=310522"
+* Add product to basket
+* View the basket and checkout trendyol "https://www.trendyol.com/sepet"
--- gauge-modanisa/tests/buy_lipstick_implementation.js
+++ gauge-modanisa/tests/buy_lipstick_implementation.js
@@ -1,11 +1,10 @@
 /* globals gauge*/
 "use strict";
-const { openBrowser, browser,$, reload, goto, press, text, contains, click, waitFor, into, inputField, below, screenshot} = require('taiko')
+const { openBrowser, goto, press, text, contains, click, waitFor, into, inputField, below, screenshot} = require('taiko')
 const assert = require("assert");
 const { setPriority } = require("os");
 const headless = process.env.headless_chrome.toLowerCase() == 'true' ? true : false;
 const delay = ms => new Promise(res => setTimeout(res,ms));
-
 
 gauge.screenshotFn = function(){
     return screenshot({encoding:"base64"})
@@ -13,23 +12,22 @@ gauge.screenshotFn = function(){
 
 step("Open the product page with <url>", async function(url){ 
 await goto(url);
-
 });
 
 step("Add product to basket", async function(){ 
  
-    
-    await click($("//button[starts-with(@class,'add-to-basket')]"));
+    await click("Sepete Ekle");
     
 });
 
-step("View the basket and checkout", async function(){ 
-    await goto("https://www.trendyol.com/sepet");
-
-    
-
-    
-    //await click("Sepeti Onayla");
-    await click($("//a[starts-with(@class,'ty-link-btn-primary')]"));
+step("View the basket and checkout <url>", async function(url){ 
+    await goto(url);
+    await click("Güvenle Satın Al")
     
     });    
+
+    step("View the basket and checkout trendyol <url>", async function(url){ 
+        await goto(url);
+        await click("Sepeti Onayla")
+        
+        });  
